6d5dc1d246e51f8952b99472073c79cf1bf521cd
HarishManeru <harish@onepgr.us>
2025-02-12 19:59:23 +0530

    sales nav url api

--- rapidAPI/fetchSalesNavURL.js
+++ rapidAPI/fetchSalesNavURL.js
@@ -361,7 +361,7 @@ router.post('/find-employees', async (req, res) => {
 
         res.status(200).json({
             status: 1,
-            data: result
+            salesNavUrl: result
         });
 
     } catch (error) {
@@ -403,7 +403,7 @@ router.get('/search-results/:requestId', async (req, res) => {
 
         res.status(200).json({
             status: 1,
-            data: result.data
+            salesNavUrl: result.data
         });
 
     } catch (error) {
