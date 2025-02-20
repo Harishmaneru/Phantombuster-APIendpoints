a4597df578d2c342edfee192e2d50235a4c41999
HarishManeru <harish@onepgr.us>
2025-02-20 16:32:55 +0530

    Implemented s3

--- virtualInterviewAPI/submissionRoutes.js
+++ virtualInterviewAPI/submissionRoutes.js
@@ -732,7 +732,7 @@ const upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: process.env.AWS_BUCKET_NAME,
-  
+    acl: 'public-read',  
     key: function (req, file, cb) {
       cb(null, `videos/${Date.now()}-${file.originalname}`);
     }
